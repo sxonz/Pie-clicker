48a2e0bc6437b67bc601d574f37297aed5b2f0bc
sxonz <taebal2020@gmail.com>
2025-06-12 17:28:36 +0900

    asset

---
+++ Assets/Script/Management/CursorChange.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class CursorChange : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Cursor.lockState
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
