90a28faadd226b30c27d27b7ff1b4ac82747e6f3
sxonz <taebal2020@gmail.com>
2025-06-16 10:34:34 +0900

    ?

---
+++ Assets/Script/Test/ButtonTest.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+    public void Clicked()
+    {
+        Debug.Log("Click");
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
